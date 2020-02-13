c04aa7c9b505c6b79941a03ae9d7ab459160a5d3
synthetic-borealis <43440983+synthetic-borealis@users.noreply.github.com>
2020-02-13 22:43:24 +0200

    S04 L05 Finishing touches

---
+++ Assets/Scripts/ScreenshotTaking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotTaking : MonoBehaviour
+{
+    [SerializeField] KeyCode _screenshotKey = KeyCode.C;
+    int _screenshotIndex = 0;
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetKeyDown(_screenshotKey))
+        {
+            if (System.IO.File.Exists("Screenshot(" + _screenshotIndex + ").png"))
+            {
+                _screenshotIndex++;
+            }
+            ScreenCapture.CaptureScreenshot("Screenshot(" + _screenshotIndex + ").png");
+            _screenshotIndex++;
+        }
+	}
+}
